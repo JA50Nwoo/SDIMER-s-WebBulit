8037597fe33f51492faaf23981dbf7ab0c28bf9f
PhilosopheAM <11911421@mail.sustech.edu.cn>
2022-04-21 16:24:29 +0800

    One_Page_HTML changes

---
+++ One_page_HTML/footer.php
@@ -0,0 +1,5 @@
+<footer class='section'>
+    <p style='color:#000; text-align:center;'>Copyright SDIM reserved</p>
+</footer>
+
+</body>
---
+++ One_page_HTML/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <?php include ('One_page_HTML/header.php'); ?>
+
+    <?php include ('One_page_HTML/footer.php'); ?>
+    
+</html>
